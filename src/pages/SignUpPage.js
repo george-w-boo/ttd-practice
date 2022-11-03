cb74f2210c4a323846723568e5d736a83a460e48
Mykhailo Ostapysko <mykhailo.ostapysko@globant.com>
2022-11-03 16:41:24 +0200

    adds sign up inputs and btn, and theri layout test cases

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,5 +1,18 @@
 function SignUpPage() {
-  return <h1>Sign Up</h1>;
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <label htmlFor="username">Username</label>
+      <input id="username" />
+      <label htmlFor="email">Email</label>
+      <input id="email" type="email" />
+      <label htmlFor="password">Password</label>
+      <input id="password" type="password" />
+      <label htmlFor="password-repeat">Password Repeat</label>
+      <input id="password-repeat" type="password" />
+      <button disabled>Sign Up</button>
+    </div>
+  );
 }
 
 export default SignUpPage;
--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -2,10 +2,56 @@ import { render, screen } from "@testing-library/react";
 
 import SignUpPage from "./SignUpPage";
 
-test("renders learn react link", () => {
-  render(<SignUpPage />);
+describe("SignUpPage", () => {
+  describe("Layout", () => {
+    it("renders learn react link", () => {
+      render(<SignUpPage />);
 
-  const headerEl = screen.getByRole("heading", { name: "Sign Up" });
+      const headerEl = screen.getByRole("heading", { name: "Sign Up" });
 
-  expect(headerEl).toBeInTheDocument();
+      expect(headerEl).toBeInTheDocument();
+    });
+
+    it("has input Username of type text", () => {
+      render(<SignUpPage />);
+
+      const usernameInputEl = screen.getByLabelText(/username/i);
+
+      expect(usernameInputEl.type).toBe("text");
+    });
+
+    it("has input Email of type email", () => {
+      render(<SignUpPage />);
+
+      const emailInputEl = screen.getByLabelText(/email/i);
+
+      expect(emailInputEl.type).toBe("email");
+    });
+
+    it("has input Password of type Password", () => {
+      render(<SignUpPage />);
+
+      const passwordInputEl = screen.getByLabelText("Password", {
+        exact: true,
+      });
+
+      expect(passwordInputEl.type).toBe("password");
+    });
+
+    it("has input Password Repeat of type Password", () => {
+      render(<SignUpPage />);
+
+      const passwordRepeatInputEl = screen.getByLabelText(/Password Repeat/i);
+
+      expect(passwordRepeatInputEl.type).toBe("password");
+    });
+
+    it("has Sign Up btn initially disabled", () => {
+      render(<SignUpPage />);
+
+      const signUpBtnEl = screen.getByRole("button", { name: /sign up/i });
+
+      expect(signUpBtnEl).toBeDisabled();
+    });
+  });
 });
