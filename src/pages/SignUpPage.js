5f4be2fbfb1e49f90947f99a22f7819ab4ebecb7
Mykhailo Ostapysko <32477362+george-w-boo@users.noreply.github.com>
2022-11-07 08:18:27 +0200

    adds sign-up page (#1)

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,136 @@
+import { useState } from "react";
+import axios from "axios";
+
+import Input from "../components/Input";
+
+function SignUpPage() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordRepeat, setPasswordRepeat] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
+  const [errors, setErrors] = useState(null);
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+
+    switch (id) {
+      case "username":
+        setUsername(value);
+        setErrors({ ...errors, username: "" });
+        break;
+      case "email":
+        setEmail(value);
+        setErrors({ ...errors, email: "" });
+        break;
+      case "password":
+        setPassword(value);
+        setErrors({ ...errors, password: "" });
+        break;
+      case "password-repeat":
+        setPasswordRepeat(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleSignUp = async (event) => {
+    event.preventDefault();
+
+    const body = {
+      username,
+      email,
+      password,
+    };
+
+    try {
+      setIsLoading(true);
+      await axios.post("/api/1.0/users", body);
+      setIsLoading(false);
+
+      setSignUpSuccess(true);
+    } catch (error) {
+      if (error.response.status === 400) {
+        setErrors(error.response.data.validationErrors);
+      }
+      setIsLoading(false);
+    }
+  };
+
+  let isSignUpBtnDisabled = true;
+
+  if (password && passwordRepeat) {
+    isSignUpBtnDisabled = password !== passwordRepeat;
+  }
+
+  return (
+    <div className="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+      {!signUpSuccess && (
+        <form className="card mt-5" data-testid="sign-up-form">
+          <div className="card-header">
+            <h1 className="text-center">Sign Up</h1>
+          </div>
+          <div className="card-body">
+            <Input
+              id="username"
+              label="Username"
+              type="text"
+              onChange={handleChange}
+              value={username}
+              helpText={errors?.username}
+            />
+            <Input
+              id="email"
+              label="Email"
+              type="email"
+              onChange={handleChange}
+              value={email}
+              helpText={errors?.email}
+            />
+            <Input
+              id="password"
+              label="Password"
+              type="password"
+              onChange={handleChange}
+              value={password}
+              helpText={errors?.password}
+            />
+            <Input
+              id="password-repeat"
+              label="Password Repeat"
+              type="password"
+              onChange={handleChange}
+              value={passwordRepeat}
+              helpText={password !== passwordRepeat ? "Passwords mismatch" : ""}
+            />
+            <div className="text-center">
+              <button
+                className="btn btn-primary"
+                disabled={isSignUpBtnDisabled || isLoading}
+                onClick={handleSignUp}
+              >
+                {isLoading && (
+                  <span
+                    className="spinner-border spinner-border-sm"
+                    role="status"
+                    aria-hidden={true}
+                  ></span>
+                )}
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </form>
+      )}
+      {signUpSuccess && (
+        <div className="alert alert-success mt-3" role="alert">
+          Please, check you email!
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SignUpPage;
---
+++ src/pages/SignUpPage.spec.js
@@ -0,0 +1,302 @@
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+// import axios from "axios";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+
+import SignUpPage from "./SignUpPage";
+
+describe("SignUpPage", () => {
+  describe("Layout", () => {
+    it("renders SignUpPage", () => {
+      render(<SignUpPage />);
+
+      const headerEl = screen.getByRole("heading", { name: "Sign Up" });
+
+      expect(headerEl).toBeInTheDocument();
+    });
+
+    it("has input Username of type text", () => {
+      render(<SignUpPage />);
+
+      const usernameInputEl = screen.getByLabelText(/username/i);
+
+      expect(usernameInputEl.type).toBe("text");
+    });
+
+    it("has input Email of type email", () => {
+      render(<SignUpPage />);
+
+      const emailInputEl = screen.getByLabelText(/email/i);
+
+      expect(emailInputEl.type).toBe("email");
+    });
+
+    it("has input Password of type Password", () => {
+      render(<SignUpPage />);
+
+      const passwordInputEl = screen.getByLabelText("Password", {
+        exact: true,
+      });
+
+      expect(passwordInputEl.type).toBe("password");
+    });
+
+    it("has input Password Repeat of type Password", () => {
+      render(<SignUpPage />);
+
+      const passwordRepeatInputEl = screen.getByLabelText(/Password Repeat/i);
+
+      expect(passwordRepeatInputEl.type).toBe("password");
+    });
+
+    it("has Sign Up btn initially disabled", () => {
+      render(<SignUpPage />);
+
+      const signUpBtnEl = screen.getByRole("button", { name: /sign up/i });
+
+      expect(signUpBtnEl).toBeDisabled();
+    });
+  });
+
+  describe("Interactions", () => {
+    let signUpBtnEl;
+    let usernameInputEl;
+    let emailInputEl;
+    let passwordInputEl;
+
+    const message = "Please, check you email!";
+
+    let requestBody;
+    let counter = 0;
+
+    const server = setupServer(
+      rest.post("/api/1.0/users", async (req, res, ctx) => {
+        requestBody = await req.json();
+
+        counter += 1;
+
+        return res(ctx.status(200));
+      })
+    );
+
+    beforeAll(() => server.listen());
+
+    beforeEach(() => {
+      counter = 0;
+      server.resetHandlers();
+    });
+
+    afterAll(() => server.close());
+
+    const setup = (password = "secret", passwordRepeat = "secret") => {
+      render(<SignUpPage />);
+
+      usernameInputEl = screen.getByLabelText(/username/i);
+      emailInputEl = screen.getByLabelText(/email/i);
+      passwordInputEl = screen.getByLabelText("Password", {
+        exact: true,
+      });
+      const passwordRepeatInputEl = screen.getByLabelText(/Password Repeat/i);
+      signUpBtnEl = screen.getByRole("button", { name: /sign up/i });
+
+      userEvent.type(usernameInputEl, "test-user");
+      userEvent.type(emailInputEl, "test@test.com");
+      userEvent.type(passwordInputEl, password);
+      userEvent.type(passwordRepeatInputEl, passwordRepeat);
+    };
+
+    it("enables sign-up btn if password and password repeat are the same", () => {
+      setup();
+
+      expect(signUpBtnEl).toBeEnabled();
+    });
+
+    // manual approach to check if sign-up submit fn has proper body
+    // it("checks if sign-up submit fn has proper body", () => {
+    //   render(<SignUpPage />);
+
+    //   const usernameInputEl = screen.getByLabelText(/username/i);
+    //   const emailInputEl = screen.getByLabelText(/email/i);
+    //   const passwordInputEl = screen.getByLabelText("Password", {
+    //     exact: true,
+    //   });
+    //   const passwordRepeatInputEl = screen.getByLabelText(/Password Repeat/i);
+    //   const signUpBtnEl = screen.getByRole("button", { name: /sign up/i });
+
+    //   userEvent.type(usernameInputEl, "test-user");
+    //   userEvent.type(emailInputEl, "test@test.com");
+    //   userEvent.type(passwordInputEl, "secret");
+    //   userEvent.type(passwordRepeatInputEl, "secret");
+
+    //   const mockFn = jest.fn();
+
+    //   axios.post = mockFn;
+
+    //   userEvent.click(signUpBtnEl);
+
+    //   const signUpFirstCall = mockFn.mock.calls[0];
+    //   const signUpSecondArgument = signUpFirstCall[1];
+
+    //   expect(signUpSecondArgument).toEqual({
+    //     username: "test-user",
+    //     email: "test@test.com",
+    //     password: "secret",
+    //   });
+    // });
+
+    it("checks if sign-up submit fn has proper body (Mock Server Worker solution)", async () => {
+      setup();
+
+      userEvent.click(signUpBtnEl);
+
+      await screen.findByText(message);
+
+      expect(requestBody).toEqual({
+        username: "test-user",
+        email: "test@test.com",
+        password: "secret",
+      });
+    });
+
+    it("checks if sign-up btn is disabled while ongoing api call", async () => {
+      setup();
+
+      userEvent.click(signUpBtnEl);
+      userEvent.click(signUpBtnEl);
+
+      await screen.findByText(message);
+
+      expect(counter).toEqual(1);
+    });
+
+    it("renders spinner after clicking sign up", async () => {
+      setup();
+
+      let spinnerEl = screen.queryByRole("status", { hidden: true });
+
+      expect(spinnerEl).not.toBeInTheDocument();
+      userEvent.click(signUpBtnEl);
+
+      spinnerEl = screen.queryByRole("status", { hidden: true });
+
+      expect(spinnerEl).toBeInTheDocument();
+
+      await screen.findByText(message);
+    });
+
+    it("renders check email alert after successful api call", async () => {
+      setup();
+
+      let emailAlertEl = screen.queryByText(message);
+
+      expect(emailAlertEl).not.toBeInTheDocument();
+
+      userEvent.click(signUpBtnEl);
+
+      emailAlertEl = await screen.findByText(message);
+
+      expect(emailAlertEl).toBeInTheDocument();
+    });
+
+    it("hides sign-up form upon successful api call", async () => {
+      setup();
+
+      const formEl = screen.getByTestId("sign-up-form");
+
+      userEvent.click(signUpBtnEl);
+
+      await waitForElementToBeRemoved(formEl);
+
+      // alternative for waitForElementToBeRemoved:
+      // await waitFor(() => {
+      //   expect(formEl).not.toBeInTheDocument();
+      // });
+    });
+
+    const generateValidationError = (field, message) => {
+      return rest.post("/api/1.0/users", async (req, res, ctx) => {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            validationErrors: {
+              [field]: message,
+            },
+          })
+        );
+      });
+    };
+
+    it("hides spinner and makes sign-up btn enabled after response received", async () => {
+      server.use(
+        generateValidationError("username", "Username cannot be null")
+      );
+
+      setup();
+
+      userEvent.click(signUpBtnEl);
+
+      const spinnerEl = screen.queryByRole("status", { hidden: true });
+      const usernameValidationErrorEl = await screen.findByText(
+        "Username cannot be null"
+      );
+
+      expect(usernameValidationErrorEl).toBeInTheDocument();
+      expect(spinnerEl).not.toBeInTheDocument();
+    });
+
+    it.each`
+      field         | message
+      ${"username"} | ${"Username cannot be null"}
+      ${"email"}    | ${"Email cannot be null"}
+      ${"password"} | ${"Password cannot be null"}
+    `("displays $message for $field", async ({ field, message }) => {
+      server.use(generateValidationError(field, message));
+
+      setup();
+
+      userEvent.click(signUpBtnEl);
+
+      const validationErrorEl = await screen.findByText(message);
+
+      expect(validationErrorEl).toBeInTheDocument();
+    });
+
+    it("displays passwords mismatch client validation error", () => {
+      setup("secret", "secret-two");
+
+      userEvent.click(signUpBtnEl);
+
+      const validationErrorEl = screen.getByText("Passwords mismatch");
+
+      expect(validationErrorEl).toBeInTheDocument();
+    });
+
+    it.each`
+      field         | message                      | label
+      ${"username"} | ${"Username cannot be null"} | ${"Username"}
+      ${"email"}    | ${"Email cannot be null"}    | ${"Email"}
+      ${"password"} | ${"Password cannot be null"} | ${"Password"}
+    `(
+      "clears $field validation error on typing",
+      async ({field, message, label}) => {
+        server.use(generateValidationError(field, message));
+
+        setup();
+
+        userEvent.click(signUpBtnEl);
+
+        const validationErrorEl = await screen.findByText(message);
+
+        userEvent.type(screen.getByLabelText(label), "updated");
+
+        expect(validationErrorEl).not.toBeInTheDocument();
+      }
+    );
+  });
+});
